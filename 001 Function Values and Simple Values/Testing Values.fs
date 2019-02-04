724db3e9938ca2d19bc5fb5553e6175f0ceacc07
Dário Santos <dariovfsantos@gmail.com>
2019-02-04 19:17:03 +0000

    Added Simple Values and Function Values

---
+++ 001 Function Values and Simple Values/Testing Values.fs
@@ -0,0 +1,21 @@
+module Testing_Values
+
+//
+//Function values
+//
+
+let add1 x = x + 1
+let plus1 = add1
+add1 5
+plus1 5
+
+//add1 and plus1 are just bounds to a value
+
+//
+//Simple values
+//
+
+let c = 5a
+add1 c
+
+//c is a value, a constant
